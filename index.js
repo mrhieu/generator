4c36f8c72984929e5a5a3d9ccdc6499250d9222b
Hieu Pham <mr_hie@yahoo.com>
2017-10-24 13:43:12 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+console.log('Hello Generator! 23423');
+
+var generator = require('custom-template-generator');
+var _ = require('lodash');
+var program = require('commander');
+
+// var name = 'button';
+// var dest = 'src/app/shared/components';
+
+program
+  .arguments('<name> [dest]')
+  .action(function (name, dest) {
+    main(name, dest);
+  });
+
+function main(name, dest) {
+  console.log('hey', name, dest);
+  name = _.startCase(name);
+
+  generator({
+    componentName: name, // <-- as {name}
+    customTemplatesUrl: './templates/',
+    dest: dest,
+    templateName: 'component', // <-- folder under ./templates/
+    autoIndent: true,
+    data: {}
+  });
+}
