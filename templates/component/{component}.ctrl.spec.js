b22a53e99a52bf3353b62feeab3d366a39be8978
Hieu Pham <mr_hie@yahoo.com>
2017-11-17 16:33:55 +0800

    chore: add spec template

---
+++ templates/component/{component}.ctrl.spec.js
@@ -0,0 +1,15 @@
+'user strict';
+
+import {Name}Ctrl from './{name}.ctrl';
+
+describe('{Name}Ctrl', () => {
+  let ctrl;
+
+  beforeEach(() => {
+    ctrl = new {Name}Ctrl();
+  })
+
+  // it('should ...', () => {
+  //   expect(1 + 1).toEqual(2);
+  // })
+})
