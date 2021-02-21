913678150cc9016436f47199d8ba251993175109
Pham Van Hieu <mr_hie@yahoo.com>
2021-02-21 19:01:41 +0800

    feat: update ionic react templates

--- src/templates/ionicReact/component/{component}.tsx
+++ src/templates/ionicReact/component/{component}.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import {
-  IonItem, IonLabel,
+
 } from '@ionic/react';
+import {
+
+} from 'ionicons/icons';
 import './{Name}.scss';
 
 type Props = {
-  isThat?: boolean,
+
 }
 
 const {Name}: React.FC<Props> = () => {
   return (
-    <div className="{kebabCase}">
-      <IonItem>
-        <IonLabel>
-          <h2>Component: {Name}</h2>
-        </IonLabel>
-      </IonItem>
-    </div>
+    <h2>My Component</h2>
   );
 };
 
 {Name}.defaultProps = {
-  isThat: true,
+
 }
 
 export default {Name};
--- src/templates/ionicReact/page/{component}.tsx
+++ src/templates/ionicReact/page/{component}.tsx
@@ -1,38 +1,26 @@
 import React from 'react';
 import {
-  IonPage, IonHeader, IonContent, IonItem, IonLabel, IonList, IonTitle, IonToolbar, IonButtons,
-  IonButton, IonBackButton, IonIcon
+  IonPage
 } from '@ionic/react';
-import { more } from 'ionicons/icons';
+import {
+
+} from 'ionicons/icons';
 import './{Name}.scss';
 
-const {Name}: React.FC<{}> = () => {
+type Props = {
+
+}
+
+const {Name}: React.FC<Props> = () => {
   return (
-    <IonPage className="{kebabCase}-page">
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonBackButton />
-          </IonButtons>
-          <IonTitle>{Name}</IonTitle>
-          <IonButtons slot="end">
-            <IonButton>
-              <IonIcon slot="icon-only" icon={{more}} mode="ios" />
-            </IonButton>
-          </IonButtons>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonList>
-          <IonItem routerLink="/some/where">
-            <IonLabel>
-              <h2>Go to detail</h2>
-            </IonLabel>
-          </IonItem>
-        </IonList>
-      </IonContent>
+    <IonPage>
+      <h2>My Component</h2>
     </IonPage>
   );
 };
+
+{Name}.defaultProps = {
+
+}
 
 export default {Name};
